f2943ee56c63d4ae44869ad8923f6bef0decb1c4
Dayne Johnston <lowtekn7@gmail.com>
2017-11-07 22:21:35 -0500

    Addition of characters component using ng

--- src/app/modules/encounters/encounters.module.ts
+++ src/app/modules/encounters/encounters.module.ts
@@ -7,12 +7,14 @@ import { EncountersComponent } from './encounters.component';
 import { MenuComponent } from './menu/index';
 
 import { AuthGuard } from '../../_guards/index';
+import { CharactersComponent } from './characters/characters.component';
 
 
 @NgModule({
   declarations: [
       EncountersComponent,
-      MenuComponent
+      MenuComponent,
+      CharactersComponent
   ],
   imports: [
     EncountersRoutingModule,
