f0b099602805b4a4b9e620d954a923322c34d394
Dayne Johnston <lowtekn7@gmail.com>
2017-12-08 23:30:09 -0500

    Start of work on the play screen

--- src/app/modules/encounters/play/play.component.ts
+++ src/app/modules/encounters/play/play.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
+  public rounds: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
   constructor() { }
 
   ngOnInit() {
